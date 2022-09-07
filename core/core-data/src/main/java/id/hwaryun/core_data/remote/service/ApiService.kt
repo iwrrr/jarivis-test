ebf41937c00e800f575592373877d66aa94c620c
Irya Muhammad <iryadev15@gmail.com>
2022-09-07 11:28:14 +0700

    update api key

--- core/core-data/src/main/java/id/hwaryun/core_data/remote/service/ApiService.kt
+++ core/core-data/src/main/java/id/hwaryun/core_data/remote/service/ApiService.kt
@@ -8,29 +8,29 @@ interface ApiService {
 
     @GET("en/API/MostPopularTVs/{apiKey}/")
     suspend fun getMostPopularTvs(
-        @Path("apiKey") apiKey: String = "k_90tcy6ha"
+        @Path("apiKey") apiKey: String = "k_k3cp3z6o"
     ): BaseResponse<PopularTvResponse>
 
     @GET("en/API/BoxOffice/{apiKey}/")
     suspend fun getBoxOfficeMovies(
-        @Path("apiKey") apiKey: String = "k_90tcy6ha"
+        @Path("apiKey") apiKey: String = "k_k3cp3z6o"
     ): BaseResponse<BoxOfficeResponse>
 
     @GET("en/API/Title/{apiKey}/{id}")
     suspend fun getMovieDetails(
-        @Path("apiKey") apiKey: String = "k_90tcy6ha",
+        @Path("apiKey") apiKey: String = "k_k3cp3z6o",
         @Path("id") movieId: String
     ): MovieDetailResponse
 
     @GET("/en/API/SearchMovie/{apiKey}/{query}/")
     suspend fun searchMovies(
-        @Path("apiKey") apiKey: String = "k_90tcy6ha",
+        @Path("apiKey") apiKey: String = "k_k3cp3z6o",
         @Path("query") query: String
     ): SearchResponse<BoxOfficeResponse>
 
     @GET("/en/API/SearchSeries/{apiKey}/{query}/")
     suspend fun searchTvs(
-        @Path("apiKey") apiKey: String = "k_90tcy6ha",
+        @Path("apiKey") apiKey: String = "k_k3cp3z6o",
         @Path("query") query: String
     ): SearchResponse<PopularTvResponse>
 }
